b8c0fa60280967d6b75a3f5f1de3d747d6df15ba
Alex <a.zarr@icloud.com>
2021-09-13 11:30:24 -0700

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 // swift-tools-version:5.4.0
+
 import PackageDescription
 
 let package = Package(
     name: "EllipticCurveKeyPair",
-    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
