c6a16183db0d23af9818053518fa5657decc7846
Alex <a.zarr@icloud.com>
2021-09-13 12:42:24 -0700

    Add platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "EllipticCurveKeyPair",
+    platforms: [
+        .iOS(.v11),
+        .watchOS(.v5)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
